a5b4ba19444f0060923bc9959c8e891a2ee2a460
cognophile <cognophile@users.noreply.github.com>
2018-05-26 14:03:45 +0100

    Updates to responsiveness and dynacism
    - Add basic functions to mock out the listing companies and have them be independantly selectable
    - Update header and footer to be sticky
    - Move bootstrap inclusion to end of files to improve loading times

--- index.php
+++ index.php
@@ -10,14 +10,10 @@
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
             <link rel="stylesheet" type="text/css" href="../assets/css/styles.css"/>
 
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
             <title> Prontostoreus - Welcome! </title>
 	</head>
 	<body>
-        <nav id="navbar" class="navbar navbar-dark">
+        <nav id="navbar" class="navbar navbar-dark fixed-top">
             <a id="nav-title" class="navbar-brand" href="index.php">
                 <img id="navbar-logo-image" src="resources/img/prontostoreus.png" width="50" height="50" class="d-inline-block align-top" alt="">
             </a>
@@ -39,15 +35,16 @@
                 </span>
             </div>
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </body>
         
     <!--Copyright-->
-    <footer id="main-footer" class="footer footer-bottom">
-        <div id="main-footer-content" class="container">
-            <p class="text-center">
-            Version <?= getVersion(); ?> <br>
-            © <?= getCopyright(); ?>
-            </p>
+    <nav id="main-footer" class="navbar fixed-bottom navbar-light bg-light">
+        <div id="main-footer-content" class="navbar-brand container">
+            <p id="main-footer-content">Version <?= getVersion(); ?> <br>© <?= getCopyright(); ?></p>
         </div>
-    </footer>
+    </nav>
 </html>
--- src/location.php
+++ src/location.php
@@ -10,24 +10,20 @@
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
             <link rel="stylesheet" type="text/css" href="../assets/css/styles.css"/>
 
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+            <script src="../assets/js/scripts.js"></script>
 
             <title> Prontostoreus - Welcome! </title>
 	</head>
 	<body>
         <section>
-            <nav id="navbar" class="navbar navbar-dark">
+            <nav id="navbar" class="navbar navbar-dark fixed-top">
                 <a id="navbar-title" class="navbar-brand" href="../index.php">
                     <img id="navbar-logo-image" src="../resources/img/prontostoreus.png" width="50" height="50" class="d-inline-block align-top" alt="">
                     <span id="navbar-title-text">Prontostoreus</span>
                 </a>
             </nav>
         </section>
-        
-        <br><br>
-    
+            
         <section>
             <div id="data-entry" class="container-fluid form-group">
                 <div id="location-container" class="container-fluid">
@@ -37,24 +33,30 @@
                 </div> 
                 <div class="row justify-content-center">
                     <span class="input-group-btn">
-                        <button id="location-submission-btn" class="browse btn btn-primary">Find companies</button>
+                        <button id="location-submission-btn" class="browse btn btn-primary" onclick="addListItem()">Find companies</button>
                     </span>
                 </div>
             </div>
+        </section>
+
+        <br><br>
+
+        <section>
+            <div id="company-list" class="list-group"></div>
         </section>
     
         </form>    
         </body>
     
         <!--Copyright-->
-        <footer id="main-footer" class="footer footer-bottom">
-            <div id="main-footer-content" class="container">
-                <p class="text-center">
-                Version <?= getVersion(); ?> <br>
-                © <?= getCopyright(); ?>
-                </p>
+        <nav id="main-footer" class="navbar fixed-bottom navbar-light bg-light">
+            <div id="main-footer-content" class="navbar-brand container">
+                <p id="main-footer-content">Version <?= getVersion(); ?> <br>© <?= getCopyright(); ?></p>
             </div>
-        </footer>
+        </nav>
                 
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	</body>
 </html>
