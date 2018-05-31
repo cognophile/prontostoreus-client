610520d4a01a72f9678b009952694ede7998a001
cognophile <cognophile@users.noreply.github.com>
2018-05-31 22:50:38 +0100

    Add global api config variables

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+const baseApi = 'http://localhost:8765/';
+
+const locationEndpoint = 'locate/';
+const customerEndpoint = 'customer/';
+const applicationEndpoint = 'apply/';
+const submissionEndpoint = 'submit/';
+const confirmationEndpoint = 'confirm/';
+const invoiceEndpoint = 'invoice/';
+
+
+
