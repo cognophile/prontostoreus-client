960a1d67f923aee6cad63060c36e9b272846c051
cognophile <cognophile@users.noreply.github.com>
2018-06-21 00:37:25 +0100

    Update form to have dynamic number of lines

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $("#start-date-input").datepicker();
     $("#end-date-input").datepicker();
+
+    addLine();
 });
 
 function showAddButton() {
@@ -11,10 +13,56 @@ function hideAddButton() {
 
 }
 
-function addLine() {
-
-}
 
 function calculateLinePrice() {
 
+}
+
+function addLine() {
+    var applicationLineHtml = "<br><div class=\"row\">" + 
+        "<div class=\"col\">" +
+            "<label id=\"room-selector-label\" class=\"field-label\" for=\"room-selector\">Room</label>" +
+            "<select id=\"room-selector\" class=\"form-control\" name=\"room\" required=\"true\">" +
+                "<option value=\"1\">Living room</option>" +
+                "<option value=\"2\">Kitchen</option>" +
+                "<option value=\"3\">Bedroom</option>" +
+                "<option value=\"4\">Bathroom</option>" +
+                "<option value=\"5\">Garage</option>" +
+            "</select>" +
+        "</div>" +
+        "<div class=\"col\">" +
+            "<label id=\"furnishing-selector-label\" class=\"field-label\" for=\"furnishing-selector\">Furnishing</label>" +
+            "<select id=\"furnishing-selector\" class=\"form-control\" name=\"furnishing\" required=\"true\">" +
+                "<option value=\"1\">TV</option>" +
+                "<option value=\"2\">Stool</option>" +
+                "<option value=\"3\">Bed</option>" +
+                "<option value=\"4\">Sofa</option>" +
+                "<option value=\"5\">Spade</option>" +
+            "</select>" +
+        "</div>" +
+
+        "<div class=\"col\">" +
+            "<label id=\"quantity-label\" class=\"field-label\" for=\"item-price-readonly\">Quantity</label>" +
+            "<input id=\"quantity-input\" class=\"form-control\" type=\"text\" name=\"quantity\" required=\"true\">" +
+        "</div>" +
+        
+        "<div class=\"col\">" +
+            "<label id=\"size-readonly-label\" class=\"field-label\" for=\"size-readonly\">Size</label>" +
+            "<p id=\"size-readonly\" class=\"form-control-static\" name=\"size\">25m2</p>" +
+        "</div>" +
+        
+        "<div class=\"col\">" +
+            "<label id=\"item-price-readonly-label\" class=\"field-label\" for=\"item-price-readonly\">Line Price</label>" +
+            "<p id=\"item-price-readonly\" class=\"form-control-static\" name=\"price\">£13.50</p>" +
+        "</div>" +
+
+        "<div class=\"col\">" +
+            "<br>" +
+            "<span class=\"input-group-btn\">" +
+                "<a id=\"add-app-line-btn\" class=\"browse btn btn-primary\" onclick=\"addLine()\">+</a>" +
+            "</span>" +
+        "</div>" +
+    "</div>";
+
+    $('#data-entry').append(applicationLineHtml);
 }
--- src/application.php
+++ src/application.php
@@ -47,65 +47,23 @@
             <div id="data-entry" class="container-fluid form-group">
                 <div class="row">
                     <div class="col">
-                        <label id="start-date-label" class="field-label" for="start-date-input">Start Date</label>
+                        <label id="collection-label" class="field-label vertical-center" for="collection-checkbox">Require collection</label>                       
+                        <input id="collection-checkbox" class="vertical-center" type="checkbox" name="collection" required="true">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label id="start-date-label" class="field-label" for="start-date-input">Storage Start Date</label>
                         <input id="start-date-input" class="form-control" type="text" name="startDate" required="true">
                     </div>
 
                     <div class="col">
-                        <label id="end-date-label" class="field-label" for="end-date-input">End Date</label>
+                        <label id="end-date-label" class="field-label" for="end-date-input">Storage End Date</label>
                         <input id="end-date-input" class="form-control" type="text" name="endDate" required="true">
                     </div>
-        
-                    <div class="col">
-                        <label id="collection-label" class="field-label" for="collection-checkbox">Collection</label>                        
-                        <input id="collection-checkbox" type="checkbox" name="collection" required="true">
-                    </div>
                 </div>
 
-                <br><br><br>
-                <div class="row">
-                    <div class="col">
-                        <label id="room-selector-label" class="field-label" for="room-selector">Room</label>
-                        <select id="room-selector" class="form-control" name="room" required="true">
-                            <option value="1">Living room</option>
-                            <option value="2">Kitchen</option>
-                            <option value="3">Bedroom</option>
-                            <option value="4">Bathroom</option>
-                            <option value="5">Garage</option>
-                        </select>
-                    </div>
-                    <div class="col">
-                        <label id="furnishing-selector-label" class="field-label" for="furnishing-selector">Furnishing</label>
-                        <select id="furnishing-selector" class="form-control" name="furnishing" required="true">
-                            <option value="1">TV</option>
-                            <option value="2">Stool</option>
-                            <option value="3">Bed</option>
-                            <option value="4">Sofa</option>
-                            <option value="5">Spade</option>
-                        </select>
-                    </div>
-
-                    <div class="col">
-                        <label id="quantity-label" class="field-label" for="item-price-readonly">Quantity</label>
-                        <input id="quantity-input" class="form-control" type="text" name="quantity" required="true">
-                    </div>
-                    
-                    <div class="col">
-                        <label id="size-readonly-label" class="field-label" for="size-readonly">Size</label>
-                        <p id="size-readonly" class="form-control-static" name="size">25m2</p>
-                    </div>
-                    
-                    <div class="col">
-                        <label id="item-price-readonly-label" class="field-label" for="item-price-readonly">Line Price</label>
-                        <p id="item-price-readonly" class="form-control-static" name="price">£13.50</p>
-                    </div>
-
-                    <div class="col">
-                        <span class="input-group-btn">
-                            <a id="next-page-btn-visible-wide" class="browse btn btn-primary" onclick="addLine()">+</a>
-                        </span>
-                    </div>
-                </div>
+                <br><br>
             </div>
         </section>
         
