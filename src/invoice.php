3dc5621232556d7ab50dd91a344cb46a4554252c
cognophile <cognophile@users.noreply.github.com>
2018-07-01 22:26:40 +0100

    Add initial invoice page

---
+++ src/invoice.php
@@ -0,0 +1,80 @@
+<?php include '../assets/php/meta.php'; ?>
+
+<!DOCTYPE html>
+<html>
+    <html lang="en">
+        <head>
+            <!-- Required meta tags -->
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            
+            <!-- Include Boostrap 4 CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+            <!-- Include custom Prontostoreus CSS -->
+            <link rel="stylesheet" type="text/css" href="../assets/css/styles.css"/>
+
+            <!-- Include jQuery JS library -->
+            <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+            <!-- Include Bootstrap 4 JS library -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+            <!-- Include Bootbox.js library  -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
+
+            <!-- Include jQuery UI widgets -->
+            <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+            <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+            <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+            
+            <!-- Include custom Prontostoreus JavaScript-->
+            <script src="../assets/js/config.js"></script>
+            <script src="../assets/js/invoice.js"></script>
+
+            <title> Prontostoreus - Invoice </title>
+	</head>
+
+	<body>
+        <section>
+            <nav id="navbar" class="navbar navbar-dark fixed-top">
+                <a id="navbar-title" class="navbar-brand" href="../index.php">
+                    <img id="navbar-logo-image" src="../resources/img/prontostoreus.png" width="50" height="50" class="d-inline-block align-top" alt="">
+                    <span id="navbar-title-text">Prontostoreus: Invoice</span>
+                </a>
+            </nav>
+        </section>
+
+        <section>
+            <div id="data-entry" class="container-fluid form-group">
+            </div>
+        </section>
+        <br><br>
+        <section>
+            <div id="meta-bottom-navbar" class="row justify-content-center">
+                <div id="bottom-btn-navbar" class="row">
+                    <div class="col-md-6">
+                        <div class="col-md-12 justify-content-center d-flex">
+                            <span class="input-group-btn">
+                                <a id="next-page-btn-visible-wide" class="browse btn btn-primary" href="javascript:history.back()">Previous</a>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="col-md-12 justify-content-center d-flex">
+                            <span class="input-group-btn">
+                                <a id="next-page-btn-visible-wide" class="browse btn btn-primary" onclick="sendApplication()">Continue</a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+    
+    <!--Copyright-->
+    <nav id="main-footer" class="navbar fixed-bottom navbar-light bg-light">
+        <div id="main-footer-content" class="navbar-brand container">
+            <p id="main-footer-content">Version <?= getVersion(); ?> <br>© <?= getCopyright(); ?></p>
+        </div>
+    </nav>
+</html>
