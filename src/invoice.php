4246f8b377a88e0774ddba02c0c5fc7e1bc7c077
cognophile <cognophile@users.noreply.github.com>
2018-07-03 01:12:26 +0100

    Add mocked page style

--- src/invoice.php
+++ src/invoice.php
@@ -45,6 +45,111 @@
 
         <section>
             <div id="data-entry" class="container-fluid form-group">
+                <div class="justify-content-center">
+                    <div>
+                        <label class="field-label-large-center">Thank you for your application!</label>
+                    </div>
+                    <div>
+                        <label class="field-label-center">We'll submit this to your chosen company and pass on your contact details. Below, you'll find a summary of your application. A pdf copy is available via the 'Download' button.</p>
+                    </div>
+                </div>
+                <br>
+                <div id="invoice-data" class="row justify-content-center">
+                    <div id="inline-sections" class="row">
+                        <div class="col-md-6">
+                            <div class="col-md-12 justify-content-center d-flex">
+                                <div id="customer-details">
+                                    <p id="customer-name">Mr. John Smith</p>
+                                    <p id="customer-address-line-one">123 Riverwood Road</p>
+                                    <p id="customer-address-line-two">n/a</p>
+                                    <p id="customer-address-town">Cambridge</p>
+                                    <p id="customer-address-county">Cambridgeshire</p>
+                                    <p id="customer-address-postcode">CB5 6GH</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="col-md-12 justify-content-center d-flex">
+                                <div id="company-details">
+                                    <p id="company-name">QuickStorage</p>
+                                    <p id="company-address-line-one">12 Cornell Road</p>
+                                    <p id="company-address-line-two">Histon Industrial Estate</p>
+                                    <p id="company-address-town">Cambridge</p>
+                                    <p id="company-address-county">Cambridgeshire</p>
+                                    <p id="company-address-postcode">CB2 4RF</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <table class="table">
+                    <thead class="thead-light">
+                        <tr>
+                            <th scope="col">Ref</th>
+                            <th scope="col">Application Number</th>
+                            <th scope="col">Customer</th>
+                            <th scope="col">Company</th>
+                            <th scope="col">Submission Date</th>
+                            <th scope="col">Invoice Issued</th>
+                            <th scope="col">Payment Due</th>
+                            <th scope="col">Terms and Conditions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">STJ20180611-1330</th>
+                            <td>253</td>
+                            <td>Mr. John Smith</td>
+                            <td>QuickStorage (Cambridge)</td>
+                            <td>17th May 2018</td>
+                            <td>20th May 2018</td>
+                            <td>21st June 2018</td>
+                            <td>Accepted</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <br><br>
+                
+                <table class="table">
+                    <thead class="thead-light">
+                        <tr>
+                        <th scope="col">Line</th>
+                            <th>Room</th>
+                            <th>Furnishing</th>
+                            <th>Item Cost</th>
+                            <th>Quantity</th>
+                            <th>Line Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                            <td>Living room</td>
+                            <td>Coffee table</td>
+                            <td>£52.99</td>
+                            <td>2</td>
+                            <td>£105.98</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                            <td>Office</td>
+                            <td>Computer</td>
+                            <td>£30.00</td>
+                            <td>1</td>
+                            <td>£30.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="col-md-12">
+                    <div class="col-md-12 justify-content-end d-flex">
+                        <div id="application-details">
+                            <label class="field-label-large">Total: £135.98</label>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
         <br><br>
@@ -62,7 +167,7 @@
                     <div class="col-md-6">
                         <div class="col-md-12 justify-content-center d-flex">
                             <span class="input-group-btn">
-                                <a id="next-page-btn-visible-wide" class="browse btn btn-primary" onclick="sendApplication()">Continue</a>
+                                <a id="next-page-btn-visible-wide" class="browse btn btn-primary" onclick="requestCreatePdf()">Download</a>
                             </span>
                         </div>
                     </div>
