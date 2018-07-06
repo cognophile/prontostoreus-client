74a550bcb71b2ce49e67741821c7f9c52c3b0f22
cognophile <cognophile@users.noreply.github.com>
2018-07-06 01:00:00 +0100

    Add population of customer and company page fields via API

--- assets/js/invoice.js
+++ assets/js/invoice.js
@@ -1,2 +1,56 @@
-// Display all the content in the page
-// Provide a button for PDF generation 
+$(document).ready(function () {
+    applicationId = getUrlParameter('application');
+    getApplicationCustomer(applicationId);
+    getApplicationCompany(applicationId);
+    getApplicationMetadata(applicationId);
+    getApplicationLines(applicationId);
+});
+
+function renderApplicationCustomer(data) {
+    var fullname = data.customer.title + " " + data.customer.firstname + " " + data.customer.surname;
+    $('#customer-name').text(fullname);
+    
+    var address = data.customer.addresses[0];
+    $('#customer-address-line-one').text(address.line_one);
+    $('#customer-address-line-two').text(address.line_two);
+    $('#customer-address-town').text(address.town);
+    $('#customer-address-county').text(address.county);
+
+    var postcode = address.postcode.split(' ').join('-');
+    $('#customer-address-postcode').text(postcode);
+}
+
+function renderApplicationCompany(data) {
+    $('#company-name').text(data.company.name);
+
+    var address = data.company.addresses[0];
+    $('#company-address-line-one').text(address.line_one);
+    $('#company-address-line-two').text(address.line_two);
+    $('#company-address-town').text(address.town);
+    $('#company-address-county').text(address.county);
+
+    var postcode = address.postcode.split(' ').join('-');
+    $('#company-address-postcode').text(postcode);
+}
+
+function getApplicationCustomer(applicationId) {
+    $.get(baseApi + invoiceEndpoint + 'applications/' + applicationId + '/customer/')
+        .done(function(response) {
+            return renderApplicationCustomer(response.data[0]);
+        })
+        .fail(function(error) {
+            renderApiError(error);
+            return false;
+        });
+}
+
+function getApplicationCompany(applicationId) {
+    $.get(baseApi + invoiceEndpoint + 'applications/' + applicationId + '/company/')
+        .done(function(response) {
+            return renderApplicationCompany(response.data[0]);
+        })
+        .fail(function(error) {
+            renderApiError(error);
+            return false;
+        });
+}
